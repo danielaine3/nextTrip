0398f80960cea90b0ede780db6c9ec6e8b361663
Dani Carter <dani.e.carter03@gmail.com>
2021-04-25 19:25:28 -0500

    remove console logs

--- src/api/getDirections.js
+++ src/api/getDirections.js
@@ -1,9 +1,9 @@
 export const getDirections = async (routeId) => {
+
   try {
     const response = await fetch(`https://svc.metrotransit.org/nextripv2/directions/${routeId}`)
-    console.log('response', response)
     const data = await response.json()
-    console.log('data', data)
+    return data
   } catch (error) {
     return error
   }   
--- src/api/getRoutes.js
+++ src/api/getRoutes.js
@@ -1,9 +1,7 @@
 export const getRoutes = async () => {
   try {
     const response = await fetch(`https://svc.metrotransit.org/nextripv2/routes/`)
-    console.log('response', response)
     const data = await response.json()
-    console.log('data', data)
     return data
   } catch (error) {
       return error
