b13b60579bda7cd24a3751746a05879821474d98
Dani Carter <dani.e.carter03@gmail.com>
2021-04-25 21:23:07 -0500

    add onChange handler to dropdown

--- src/Components/Dropdown/Dropdown.jsx
+++ src/Components/Dropdown/Dropdown.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 export const Dropdown = ({
   id,
   options, 
+  onSelect,
+  selected,
   title,
 }) => {
+  const handleChange = (event) => {
+    onSelect(event.target.value)
+  }
   console.log('options', options)
 
 
@@ -13,7 +18,8 @@ export const Dropdown = ({
       <label id={`${id}-label`}>{title}</label>
       <select
         id={id}
-        labelId={`${id}-label`}
+        onChange={handleChange}
+        value={selected}
       >
         <option key={'key1'} value={'value1'}>Route1</option>
         <option key={'key2'} value={'value2'}>Route2</option>
