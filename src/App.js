edc866c528bc87aa8337dcb02e96000ddb8a317e
Dani Carter <dani.e.carter03@gmail.com>
2021-04-25 20:25:28 -0500

    add direction and stops dropdowns

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react'
-import { getDirections, getRoutes, getStops } from './api'
+import { fetchDirections, fetchRoutes, fetchStops } from './api'
 import { Dropdown } from './Components/Dropdown'
-import './App.css';
-
+import './App.css'
 
 export const App = () => {
-  const routes = getRoutes()
-  getDirections(902)
-  getStops(902, 1)
+  const routes = fetchRoutes()
+  const directions = fetchDirections(902)
+  const stops = fetchStops(902, 1)
 
   return (
     <div className="App">
       <Dropdown 
         id={'id'}
         options={routes}
-        title={'title'}
+        title={'Select route'}
+      />
+      <Dropdown 
+        id={'id'}
+        options={directions}
+        title={'Select direction'}
+      />
+      <Dropdown 
+        id={'id'}
+        options={stops}
+        title={'Select stop'}
       />
     </div>
   );
