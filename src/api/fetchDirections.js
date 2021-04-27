c70951796fddd2a578d145fbc3d3cabf3f331a07
Dani Carter <dani.e.carter03@gmail.com>
2021-04-26 20:18:54 -0500

    fix controlled to uncontrolled component error

--- src/api/fetchDirections.js
+++ src/api/fetchDirections.js
@@ -2,7 +2,10 @@ export const fetchDirections = async (routeId) => {
   try {
     const response = await fetch(`https://svc.metrotransit.org/nextripv2/directions/${routeId}`)
     const data = await response.json()
-    return data
+    return data.map(datum => ({
+      ...datum,
+      direction_id: datum.direction_id.toString()
+    }))
   } catch (error) {
     return error
   }   
