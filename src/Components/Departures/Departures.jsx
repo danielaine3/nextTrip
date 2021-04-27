612351ac94b60d1d656fe1dd5c5f69edb0598c70
Dani Carter <dani.e.carter03@gmail.com>
2021-04-27 13:20:32 -0500

    add stop information styles

--- src/Components/Departures/Departures.jsx
+++ src/Components/Departures/Departures.jsx
@@ -2,17 +2,33 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useDepartures } from '../../hooks/useDepartures'
 import { 
+  makeStyles,
   Paper,
   TableContainer,
   Table,
   TableBody,
   TableHead,
   TableRow,
-  TableCell
+  TableCell,
+  Typography, 
+
 } from '@material-ui/core'
 import { Alert } from '@material-ui/lab'
 
+const useStyles = makeStyles(() => ({
+  stopInfo: {
+    color: 'white',
+    backgroundColor: 'green',
+    alignItems: 'center',
+    padding: 10
+  },
+  stopName: {
+    fontSize: '20px'
+  }
+}))
+
 export const Departures = () => {
+  const classes = useStyles()
   const { routeId, directionId, stopId } = useParams()
   const departureData = useDepartures(routeId, directionId, stopId)
 
@@ -23,31 +39,33 @@ export const Departures = () => {
   )
 
   return (
-    <TableContainer component={Paper} data-testid='departures-table'>
-      <Table>
-        <TableHead>
-          {departureData.stops.map(stop => (
-            <TableRow key={stop.stop_id}>
-              <TableCell align='left'>{stop.description}</TableCell>
-              <TableCell align='right'>Stop #:{stop.stop_id}</TableCell>
+    <>
+      {departureData.stops.map(stop => (
+        <Paper className={classes.stopInfo} key={stop.stop_id} elevation={3}>
+          <Typography align='center' className={classes.stopName}>{stop.description}</Typography>
+          <Typography align='center'>Stop #:{stop.stop_id}</Typography>
+        </Paper>
+      ))}
+      <TableContainer component={Paper} data-testid='departures-table'>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align='left'>Route</TableCell>
+              <TableCell align='left'>Destination</TableCell>
+              <TableCell align='right'>Departs</TableCell>
             </TableRow>
-          ))}
-          <TableRow>
-            <TableCell align='left'>Route</TableCell>
-            <TableCell align='left'>Destination</TableCell>
-            <TableCell align='right'>Departs</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {departureData.departures.map(departure => (
-            <TableRow key={departure.trip_id}>
-              <TableCell align='left'>{departure.route_short_name}</TableCell>
-              <TableCell align='left'>{departure.description}</TableCell>
-              <TableCell align='right'>{departure.departure_text}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {departureData.departures.map(departure => (
+              <TableRow key={departure.trip_id}>
+                <TableCell align='left'>{departure.route_short_name}</TableCell>
+                <TableCell align='left'>{departure.description}</TableCell>
+                <TableCell align='right'>{departure.departure_text}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   )
 }
